d31080e768473fdd0e634f29d29190987b1cb381
Rafael Cordero <recordero04@ucatolica.edu.co>
2025-12-01 19:09:27 -0500

    feat(db): Configuración inicial de conexión asincrona a PostgresSQL Utilizando SUPABASE
    Se implementa la conexión a la base de datos PostgreSQL
    
    Detalles clave:
    - Uso de variables de entorno para las credenciales de conexión (seguridad).
    - Definición de 'AsyncEngine' para operaciones asíncronas.
    - Función 'create_tables' para inicializar el esquema en el arranque de la aplicación.
    - Implementación de la dependencia 'get_session' para inyección de sesión en las rutas de FastAPI."

--- db.py
+++ db.py
@@ -1,19 +1,38 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, Depends
+from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel, create_engine, Session
 from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine, create_async_engine
 
-db_name = "SpiderMans.sqlite3"
-db_url = f"sqlite:///{db_name}"
+load_dotenv()
 
-engine = create_engine(db_url)
+SUPABASE_DB = (
+    f"postgresql+asyncpg://{os.getenv('POSTGRESQL_ADDON_USER')}:"
+    f"{os.getenv('POSTGRESQL_ADDON_PASSWORD')}@"
+    f"{os.getenv('POSTGRESQL_ADDON_HOST')}:"
+    f"{os.getenv('POSTGRESQL_ADDON_PORT')}/"
+    f"{os.getenv('POSTGRESQL_ADDON_DB')}"
+)
 
-def create_tables(app:FastAPI):
-    SQLModel.metadata.create_all(engine)
-    yield
+##db_name = "SpiderMans.sqlite3"
+##db_url = f"sqlite:///{db_name}"
 
-def get_session() -> Session:
-    with Session(engine) as session:
+engine : AsyncEngine = create_async_engine(SUPABASE_DB, echo=True)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+##Engine con SQL
+##engine = create_engine(db_url)
+
+async def create_tables(app:FastAPI):
+    print("Iniciando creación de tablas (Alembic o SQLModel)...")
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session():
+    async with async_session() as session:
         yield session
 
-SessionDep= Annotated[Session, Depends(get_session)]
+SessionDep= Annotated[async_session, Depends(get_session)]
 
