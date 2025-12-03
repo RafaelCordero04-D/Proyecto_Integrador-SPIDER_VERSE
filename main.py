e042fd74ea28c8820573033f5ebdfbd2ca9bb29d
Rafael Cordero <recordero04@ucatolica.edu.co>
2025-12-03 13:19:04 -0500

    feat: Setup basic FastAPI structure, static files, and templates
    - Inicializar la aplicación de FASTAPI con el titulo "Spiderman API".
    - Añadir `asynccontextmanager` Para la aplicacion de lifespan (prepares for database/startup tasks).
    - Mount `StaticFiles` for the `TemplatesHTML` directory.
    - Configurar `Jinja2Templates` para renderizar el HTML
    - Importar las librerias necesarias para su uso futuro (UploadFile, File, Form, Request, HTTPException).

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form, Request, HTTPException
+from contextlib import asynccontextmanager
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 import Spiderman
 import universe
 import pelicula
 from db import create_tables
 
-app = FastAPI(lifespan= create_tables, title="Spiderman API")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables(app)
+    yield
+app = FastAPI(lifespan= lifespan, title="Spiderman API")
+
+app.mount("/TemplatesHTML", StaticFiles(directory="TemplatesHTML"), name="TemplatesHTML")
 app.include_router(Spiderman.router, tags=["SpiderMan"], prefix="/SpiderMans")
 app.include_router(universe.router, tags=["universe"], prefix="/universes")
-
 app.include_router(pelicula.router, tags=["pelicula"], prefix="/peliculas")
 
+
+Templates = Jinja2Templates(directory="TemplatesHTML")
 
 @app.get("/")
 async def root():
@@ -23,3 +35,5 @@ async def say_hello(name: str):
 @app.get("/nombre/{name]")
 async def say_yourName(name: str):
     return {"presentation": f"Mi nombre es {name}"}
+
+
